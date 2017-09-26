1218de6d1a7a3aaeddffd0401530c468cae0ae00
Dextter <pablo-s_fictions@outlook.com>
2017-09-26 20:12:37 -0300

    migração de mysql para postgres

--- public/asset/production/login.php
+++ public/asset/production/login.php
@@ -2,9 +2,9 @@
 
     //Linha de código para retirar a advertência da depreciação do MySql no PHP.
     error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-    //Conexão com o banco de dados MySql; o or die é para verificar se há erro de conexão.
-    mysql_connect('localhost','root','') or die(mysql_error());
-    mysql_select_db('easytour') or die(mysql_error());
+    //Conexão com o banco de dados; o or die é para verificar se há erro de conexão.
+    $myPDO = new PDO('pgsql:host=localhost;dbname=easytour', 'easytour', 'easytour') or die(pg_last_error());
+    $result = $myPDO->query("SELECT * FROM usuarios") or die(pg_last_error());
 
     //Verifica se há cadastro no login do usuário.
     if(isset($_POST['login']) && isset($_POST['senha'])){
@@ -14,14 +14,14 @@
         $senha = $_POST['senha'];
         
         //Verifica o tipo de usuário.
-        $get = mysql_query("SELECT * FROM usuarios WHERE email = '$login' AND senha = '$senha'");
+         $get = $myPDO->query("SELECT * FROM usuarios WHERE email = '$login' AND senha = '$senha'");
         //Conta o número de registro que tem no banco.
-        $num = mysql_num_rows($get);
+        $num = pg_num_rows($get);
         
         //Se $num for igual a 1, é porque existe o usuário na tabela, se não será 0.
         if($num == 1){
             //O laço percorrerá os campos da tabela para encontrar o usuário.
-            while($percorrer = mysql_fetch_array($get)){
+            while($percorrer = pg_fetch_array($get)){
                 //A pesquisa será pelo campo adm do banco de dados.
                 $adm = $percorrer['adm'];
                 $nome = $percorrer['nome'];
